30197450220761abba58b7fcca64c722c2e5ab34
Julio Montero <38279248+efervescencia@users.noreply.github.com>
2025-02-08 00:54:50 +0100

    fix of corr and apply style

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
+input_values = [1,2,3,4,5]
 squares = [1,4,9,16,25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+#print(plt.style.available)
+plt.style.use('seaborn-v0_8')
+ax.plot(input_values, squares)
 
 plt.show()
 
