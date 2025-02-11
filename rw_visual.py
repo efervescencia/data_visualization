4697d9a4752835deb4a3b449e11d8467632047ae
Julio Montero <38279248+efervescencia@users.noreply.github.com>
2025-02-11 01:31:44 +0100

    color map in random walk

--- rw_visual.py
+++ rw_visual.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
+while True:
+    #hacemos un camino aleatorio
+    rw = RandomWalk()
+    rw.fill_walk()
 
-#hacemos un camino aleatorio
-rw = RandomWalk()
-rw.fill_walk()
+    #trazamos los puntos del camino
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c= point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    plt.show()
 
-#trazamos los puntos del camino
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
+
