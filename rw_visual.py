0e36e6a725b11541c02354d717ce8a9ea64fb611
Julio Montero <38279248+efervescencia@users.noreply.github.com>
2025-02-11 01:39:39 +0100

    añadimos 50.000 puntos de trazado

--- rw_visual.py
+++ rw_visual.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 while True:
     #hacemos un camino aleatorio
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     #trazamos los puntos del camino
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c= point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c= point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     #eliminamos los ejes
     ax.get_xaxis().set_visible(False)
