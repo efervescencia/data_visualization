7ad1b0da158272fd1a7afe0758b70197f33d1461
Julio Montero <38279248+efervescencia@users.noreply.github.com>
2025-02-13 01:26:24 +0100

    polen in water

--- rw_visual.py
+++ rw_visual.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
+from sympy.printing.pretty.pretty_symbology import line_width
 
 from random_walk import RandomWalk
 while True:
     #hacemos un camino aleatorio
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
     #trazamos los puntos del camino
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(15,9), dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c= point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values)
 
     #eliminamos los ejes
     ax.get_xaxis().set_visible(False)
