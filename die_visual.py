b44ae0c95335b40a695caf4b5f6ebeda9739350c
Julio Montero <38279248+efervescencia@users.noreply.github.com>
2025-02-21 18:07:28 +0100

    frequency analisys

--- die_visual.py
+++ die_visual.py
@@ -5,8 +5,13 @@ die = Die()
 
 #hace algunas tiradas y guarda los resultados en una lista
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+#analizamos los resultados
+frequencies = []
+for value in range(1,die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+print(frequencies)
