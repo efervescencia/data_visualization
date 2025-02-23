940845eb7edf9dda2e9abd9d09005df39410ab61
Julio Montero <38279248+efervescencia@users.noreply.github.com>
2025-02-23 20:13:53 +0100

    errors handled

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,9 +1,36 @@
 import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
 
 filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    dates, highs, lows = [], [], []
+
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f'Missing data for {current_date}')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+#Traza las temperaturas maximas y minimas
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.plot(dates, highs, c='red', alpha=0.5)
+    ax.plot(dates, lows, c='blue', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+#da formato al trazado
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+
+plt.show()
