702c5b76d719975c17e3fdbe223e1168ccd223f4
Julio Montero <38279248+efervescencia@users.noreply.github.com>
2025-07-06 01:03:29 +0200

    añadido magnitud y geoposicion

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -7,3 +7,19 @@ with open(filename, encoding='utf-8') as f:
 readable_file = 'data/readable_eq_data.json'
 with open(readable_file, "w", encoding="utf-8") as f:
     json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+mags, longs, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    longs.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(longs[:10])
+print(lats[:10])
